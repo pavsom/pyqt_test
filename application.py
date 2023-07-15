7eb502146473e839886d17947e4f1c7f70f4c465
pavsom <pavsome@mail.ru>
2023-07-15 18:01:46 +0300

    Добавлены первые слоты и сигналы

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtGui import QColor, QFont
 
 import sys
 
@@ -9,9 +10,23 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("Test Application")
+
         button = QPushButton("Test button")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
+        button.clicked.connect(self.the_button_was_toggled)
+
+        self.setMinimumSize(QSize(250, 190))
+        self.setMaximumSize(QSize(777, 666))
+        self.setFont(QFont("Times", 10))
 
         self.setCentralWidget(button)
+
+    def the_button_was_clicked(self):
+        print("Button clicked...")
+
+    def the_button_was_toggled(self, checked):
+        print("Checked?", checked)
 
 
 app = QApplication(sys.argv)
